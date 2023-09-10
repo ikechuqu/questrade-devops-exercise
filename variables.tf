75da7911dee7b1f8da832b8abfa327d602b0fe04
Ikechukwu Jerome <ikechukwuajerome@gmail.com>
2023-09-10 13:35:48 -0400

    chore: update resource variables

---
+++ variables.tf
@@ -0,0 +1,68 @@
+// Provider Variables
+
+variable "credentials" {
+  type = string
+  description = "GCP service account credentials file path"
+  sensitive = true
+}
+
+variable "project-id" {
+  type = string
+  description = "GCP project id"
+}
+
+variable "region" {
+  type = string
+  description = "Resource deployment region"
+}
+
+variable "zone" {
+  type = string
+  description = "Resource deployment zone"
+}
+
+// Network Variables
+
+variable "questrade_network" {
+  type = string
+  description = "Custom VPC network"
+  default = "questrade-net"
+}
+
+variable "subnet_name" {
+  type = string
+  description = "VPC subnet name"
+  default = "questrade-subnet"
+}
+
+variable "subnet_cidr_range" {
+  type = string
+  description = "IP cidr range for subnet"
+}
+
+// Virtual Machine
+
+variable "instance_type" {
+  type = string
+  description = "Virtual machine instance type"
+  default = "n2-standard-2"
+}
+
+variable "instance_image" {
+  type = string
+  description = "Virtual machine instance image"
+  default = "ubuntu-os-cloud/ubuntu-2204-lts"
+}
+
+// SSH Credentials
+
+variable "ssh_user" {
+  type = string
+  description = "SSH username"
+}
+
+variable "ssh_key" {
+  type = string
+  description = "SSH public key file path"
+  sensitive = true
+}
