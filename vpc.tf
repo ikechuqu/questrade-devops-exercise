6ef49d32e2d947b35b0ac2d79e27ab63283100bf
Ikechukwu Jerome <ikechukwuajerome@gmail.com>
2023-09-10 20:14:48 -0400

    chore: update subnets configuration

--- vpc.tf
+++ vpc.tf
@@ -1,18 +1,19 @@
 // Configure custom vpc module
 module "vpc" {
-  source =  "terraform-google-modules/network/google"
-  version = "~> 7.0"
-  project_id = var.project-id
+  source                  = "terraform-google-modules/network/google"
+  version                 = "~> 7.0"
+  project_id              = var.project-id
   auto_create_subnetworks = false
 
   network_name = var.vpc_name
   routing_mode = "REGIONAL"
 
   subnets = [
+    for subnet in var.subnets :
     {
-        subnet_name = var.subnet_name
-        subnet_ip = var.subnet_cidr_range
-        subnet_region = var.region
+      subnet_name   = subnet.subnet_name
+      subnet_ip     = subnet.subnet_ip
+      subnet_region = subnet.subnet_region
     }
   ]
 }
